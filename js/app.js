1fd7c51165ccc268ca5645bf5b69872bc04e9857
Selection070 <d.dvoeryadkin@outlook.com>
2024-08-06 17:48:07 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -34,7 +34,6 @@ order.addEventListener("click", () => {
     let email = document.getElementById("user_email").value;
     let additional_info = document.getElementById("additional_info").value;
     let appeal = document.getElementById("appeal").value;
-    let callbacktype = document.getElementById("callbacktype").value;
     document.getElementById("error").InnerText = '';
 
     if(user_name.length  < 3)  {
